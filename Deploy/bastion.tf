2137a59d7eccf8261a38071882e452507eab6ce4
Sharwari <deshmukh.sharwari03@gmail.com>
2021-01-31 15:05:01 +0530

    added bastion user data

---
+++ Deploy/bastion.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "amazon_linux"{
+    most_recent = true
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+
+    }
+    owners = ["amazon"]
+}
+
+resource "aws_instance" "bastion"{
+    ami           = data.aws_ami.amazon_linux.id
+    instance_type = "t1.micro"
+    user_data     = file("./templates/bastion/user-data.sh")
+
+    tags = merge(
+        local.common_tags,
+        map("Name", "${local.prefix}-bastion")
+    )
+}

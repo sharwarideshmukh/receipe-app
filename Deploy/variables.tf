cc1974195c0b1cf85d57b166c3fc57e7719126b4
Sharwari <deshmukh.sharwari03@gmail.com>
2021-01-31 09:18:04 +0530

    Added variables

--- Deploy/variables.tf
+++ Deploy/variables.tf
@@ -1,4 +1,14 @@
-# ... existing code ...
+variable "prefix" {
+  default = "raad"
+}
+
+variable "project"{
+  default = "receipe-app- api-devops"
+}
+
+variable "contact"{
+  default = "dsharwari202@gmail.com"
+}
 
 variable "db_username" {
   description = "Username for the RDS Postgres instance"
